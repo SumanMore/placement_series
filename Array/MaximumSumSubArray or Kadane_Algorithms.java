8cc11269c917ad27ce0d04848c64f4b99b211ee0
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-02-26 13:24:39 +0530

    Update MaximumSumSubArray or Kadane_Algorithms.java

--- Array/MaximumSumSubArray or Kadane_Algorithms.java
+++ Array/MaximumSumSubArray or Kadane_Algorithms.java
@@ -15,3 +15,26 @@ int maxsubArraysum(int a[],int size)
     }
     return max_so_far;
   }}
+
+
+int maxsubArraysum(int a[],int size)
+{
+  int start=0,end=0,s=0;
+  int max_so_far=Integer.MAX_VALUE;
+  int max_ending_here=0;
+  for(int i=0;i<size;i++)
+  {
+    max_ending_here=max_ending_here+a[i];
+    if(max_so_far<max_ending_here)
+    {
+      max_so_far=max_ending_here;
+      start=s;
+      end=i;
+    }
+    if(max_ending_here<0)
+    {
+      max_ending_here=0;
+      s=i+1;
+    }
+    return max_so_far;
+  }}
