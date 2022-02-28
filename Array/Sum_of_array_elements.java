fc833b935a47669fa78bc0fa0835fa816158db15
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-02-28 23:53:18 +0530

    Create Sum_of_array_elements.java

---
+++ Array/Sum_of_array_elements.java
@@ -0,0 +1,13 @@
+class Get
+{
+    public static int sumElement(int arr[], int n)
+    {
+        // Your code here
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+arr[i];
+        }
+        return sum;
+    }
+}
