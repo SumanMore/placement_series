47cbe7b5d8616f0693338071b935799aa41d03cc
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-01 00:16:32 +0530

    Create Find_transition_point.java

---
+++ Array/Find_transition_point.java
@@ -0,0 +1,11 @@
+class GfG {
+    int transitionPoint(int arr[], int n) {
+        // code here
+        for(int i=0;i<n;i++)
+          {
+              if(arr[i]==1)
+                 return i;
+          }
+         return -1;  
+    }
+}
