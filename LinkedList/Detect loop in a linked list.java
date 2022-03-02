fe4cf807c288e3966fabecd702a567c5b9a1bcb6
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-02 23:22:29 +0530

    Update Detect loop in a linked list.java

--- LinkedList/Detect loop in a linked list.java
+++ LinkedList/Detect loop in a linked list.java
@@ -22,3 +22,26 @@ Node check_loop(Node head)
     }
     return p;
   }
+
+  
+  
+  
+  or
+
+    
+    class Solution {
+    //Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head){
+        // Add code 
+        HashSet<Node> hs=new HashSet<>();
+        while(head!=null)
+        {
+            if(hs.contains(head)) 
+               return true;
+            hs.add(head);
+            head = head.next;
+         }
+        return false;
+        
+    }
+}
