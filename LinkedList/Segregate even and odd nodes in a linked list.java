defb0df02cc39c555dda91dcfc44a29c6fab0c40
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-05 22:58:41 +0530

    Create Segregate even and odd nodes in a linked list.java

---
+++ LinkedList/Segregate even and odd nodes in a linked list.java
@@ -0,0 +1,55 @@
+class Solution{
+    Node divide(int N, Node head){
+        // code here
+        Node even=null;
+        Node odd=null;
+        Node e=null;
+        Node o=null;
+        while(head!=null)
+        {
+            if(head.data%2==0)
+            {
+                if(even==null)
+                {
+                even=head;
+                e=head;
+                }
+            else
+            {
+                e.next=head;
+                e=e.next;
+            }
+            }
+            else
+            {
+                if(odd==null)
+                {
+                odd=head;
+                o=head;
+                }
+            else
+            {
+                o.next=head;
+                o=o.next;
+            }
+            }
+        
+            head=head.next;
+    }
+            if(e!=null)
+            {
+                e.next=odd;
+            }
+            if(o!=null)
+            {
+                o.next=null;
+            }
+            if(even!=null)
+            {
+                return even;
+            }
+            return odd;
+            
+        
+    }
+}
