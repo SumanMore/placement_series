31e745fd58a768dc84e91c96d9bd7c4972bfbef0
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-06 15:52:18 +0530

    Update Find a triplet that sum to a given value.java

--- Array/Find a triplet that sum to a given value.java
+++ Array/Find a triplet that sum to a given value.java
@@ -24,29 +24,28 @@ boolean find3Numbers(int A[], int arr_size, int sum)
 
 or
 
-static boolean find3Numbers(int A[],
-                                int arr_size, int sum)
+class Solution
+{
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    public static boolean find3Numbers(int A[], int n, int X) { 
+    
+       // Your code Here
+       int j;
+    for(int i=0;i<n-2;i++)
     {
-        // Fix the first element as A[i]
-        for (int i = 0; i < arr_size - 2; i++) {
- 
-            // Find pair in subarray A[i+1..n-1]
-            // with sum equal to sum - A[i]
-            HashSet<Integer> s = new HashSet<Integer>();
-            int curr_sum = sum - A[i];
-            for (int j = i + 1; j < arr_size; j++)
+        HashSet<Integer> ob=new HashSet<Integer>();
+        int curr=X-A[i];
+        for( j=i+1;j<n;j++)
+        {
+            if(ob.contains(curr-A[j]))
             {
-                if (s.contains(curr_sum - A[j]))
-                {
-                    System.out.printf("Triplet is %d,
-                                        %d, %d", A[i],
-                                      A[j], curr_sum - A[j]);
-                    return true;
-                }
-                s.add(A[j]);
+                return true;
             }
+             ob.add(A[j]);
         }
- 
-        // If we reach here, then no triplet was found
-        return false;
+       
     }
+    return false;
+    }
+}
