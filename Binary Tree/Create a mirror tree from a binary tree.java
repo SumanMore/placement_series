f5125bb12bccf57246366c3a12153a4a97c5f420
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-07 23:23:53 +0530

    Update Create a mirror tree from a binary tree.java

--- Binary Tree/Create a mirror tree from a binary tree.java
+++ Binary Tree/Create a mirror tree from a binary tree.java
@@ -1,12 +1,16 @@
-void mirror(Node root)
-{
-  if(root)
-  {
-    mirror(root.left);
-    mirror(root.right);
-    Node temp=root.left;
-    root.left=root.right;
-    root.right=temp;
-  }
-  return ;
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+        if(node!=null)
+        {
+            mirror(node.left);
+            mirror(node.right);
+            Node temp=node.left;
+            node.left=node.right;
+            node.right=temp;
+            
+        }
+        return ;
+    }
 }
