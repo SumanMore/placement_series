5f8112c46940a36dc6bdc6ab756f9399453b9173
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-10 00:09:19 +0530

    Update Factorial of large numbers.java

--- Array/Factorial of large numbers.java
+++ Array/Factorial of large numbers.java
@@ -10,3 +10,27 @@ static BigInteger factorial(int N)
  
         return f;
     }
+
+or
+class Solution {
+    static ArrayList<Integer> factorial(int N){
+       
+         ArrayList<Integer> arrayList = new ArrayList<>();
+
+       java.math.BigInteger num = new java.math.BigInteger("1");
+
+       for (int i = 2; i <= N; i++) {
+           num = num.multiply(java.math.BigInteger.valueOf(i));
+       }
+
+       char[] charArray = num.toString().toCharArray();
+
+       for (char c : charArray) {
+           int i = Integer.parseInt(String.valueOf(c));
+           arrayList.add(i);
+       }
+
+       return arrayList;
+ 
+    }
+}
