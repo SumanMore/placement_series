e1ef73fdc11f5b9744803642c75c4c1ba84efec8
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-09 23:12:36 +0530

    Create Merge 2 sorted arrays without extra space.java

---
+++ Array/Merge 2 sorted arrays without extra space.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public void merge(int arr1[], int arr2[], int n, int m) {
+        // code here
+        int i=n-1;
+        int j=0;
+        while(i>=0&&j<m)
+        {
+            if(arr1[i]>arr2[j])
+            {
+            int temp=arr1[i];
+            arr1[i]=arr2[j];
+            arr2[j]=temp;
+            }
+            i--;
+            j++;
+         }
+         Arrays.sort(arr1);
+         Arrays.sort(arr2);
+}}
