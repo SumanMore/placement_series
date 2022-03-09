5739d2e9284983a184486f8fdd45b6c856051d0d
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-10 00:08:04 +0530

    Create Factorial of large numbers.java

---
+++ Array/Factorial of large numbers.java
@@ -0,0 +1,12 @@
+static BigInteger factorial(int N)
+    {
+        // Initialize result
+        BigInteger f
+            = new BigInteger("1"); // Or BigInteger.ONE
+ 
+        // Multiply f with 2, 3, ...N
+        for (int i = 2; i <= N; i++)
+            f = f.multiply(BigInteger.valueOf(i));
+ 
+        return f;
+    }
