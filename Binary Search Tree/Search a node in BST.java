546f57f84ff259a92141e99d00d85ec9d1d00b46
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-11 22:45:13 +0530

    Create Search a node in BST.java

---
+++ Binary Search Tree/Search a node in BST.java
@@ -0,0 +1,19 @@
+class BST {
+    // Function to search a node in BST.
+    boolean search(Node root, int x) {
+        // Your code here
+        if(root==null)
+        {
+            return false;
+        }
+        if(root.data==x)
+        {
+            return true;
+        }
+        if(root.data<x)
+        {
+            return search(root.right,x);
+        }
+        return search(root.left,x);
+    }
+}
