466341b0c9be55e2fcbb4d812ca4535ca982db1f
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-14 01:26:13 +0530

    Create Rotate Array.java

---
+++ Array/Rotate Array.java
@@ -0,0 +1,9 @@
+for(int i=rotatingindex;i<size;i++)
+     {
+         System.out.print(array[i]+" ");
+     }
+     
+      for(int i=0;i<rotatingindex;i++)
+     {
+         System.out.print(array[i]+" ");
+     }
