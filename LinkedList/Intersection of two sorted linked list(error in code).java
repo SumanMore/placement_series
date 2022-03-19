566092be3ccd166af237703d9eeee8f42ab9db73
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-19 11:27:05 +0530

    Update Intersection of two sorted linked list(error in code).java

--- LinkedList/Intersection of two sorted linked list(error in code).java
+++ LinkedList/Intersection of two sorted linked list(error in code).java
@@ -55,3 +55,36 @@ class Sol
         return temp1;
     }
 }
+
+//correct one 
+class Sol
+{
+   public static Node findIntersection(Node head1, Node head2)
+    {
+         Node dummy=new Node(-1);
+       Node ans=dummy;
+       Node temp1=head1;
+       Node temp2=head2;
+       while(temp1!=null && temp2!=null)
+       {
+           if(temp1.data==temp2.data)
+           {
+               Node forward=new Node(temp1.data);
+               dummy.next=forward;
+               temp1=temp1.next;
+               temp2=temp2.next;
+               dummy=dummy.next;
+           }
+           else if(temp1.data>temp2.data)
+           {
+               temp2=temp2.next;
+           }
+           else
+           {
+               temp1=temp1.next;
+           }
+           
+       }
+       return ans.next;
+    }
+}
