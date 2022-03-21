c7ef091d71a8be13423da3d92f41548c61bde2ab
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-22 00:39:27 +0530

    Update Reverse a string using stack.java

--- Stack and Queues/Reverse a string using stack.java
+++ Stack and Queues/Reverse a string using stack.java
@@ -20,3 +20,23 @@ class Solution {
     }
 
 }
+
+or
+class Solution {
+    
+    public String reverse(String S){
+        //code here
+        String str="";
+        Stack<Character> ob=new Stack<Character>();
+        for(int i=0;i<S.length();i++)
+        {
+            ob.push(S.charAt(i));
+        }
+         for(int i=0;i<S.length();i++)
+        {
+           str=str+ob.pop();
+        }
+        return str;
+    }
+
+}
