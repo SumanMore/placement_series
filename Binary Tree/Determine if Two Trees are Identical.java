8e7b0a4ddac739583a918d9c2064650111e4bbe0
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-23 22:53:31 +0530

    Create Determine if Two Trees are Identical.java

---
+++ Binary Tree/Determine if Two Trees are Identical.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    //Function to check if two trees are identical.
+	boolean isIdentical(Node root1, Node root2)
+	{
+	    // Code Here
+	    if (root1 == null && root2 == null) {
+        return true;
+    } else if (root1 == null || root2 == null) {
+        return false;
+    } else if (root1.data != root2.data) {
+        return false;
+    }
+    
+    return isIdentical(root1.left, root2.left) & isIdentical(root1.right, root2.right);
+	}
+	
+}
