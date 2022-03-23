0568f24605deca5debb8b11eb358d47a66570094
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-23 13:15:14 +0530

    Update Next_Permutation.java

--- Array/Next_Permutation.java
+++ Array/Next_Permutation.java
@@ -26,3 +26,42 @@ public static void nextPermutation( List<Integer> perm)
   System.out.println(perm);
   
 }}
+
+// or
+class Solution{
+    static List<Integer> nextPermutation(int N, int arr[]){
+        
+        ArrayList<Integer> perm=new ArrayList<Integer>();
+        for(int i=0;i<arr.length;i++)
+        {
+        perm.add(arr[i]);
+        }
+        int k=arr.length-2;
+        while(arr[k]>=arr[k+1])
+        {
+            k=k-1;
+            if(k<0)
+            {
+                break;
+            }
+        }
+        if(k==-1)
+        {
+           Collections.reverse(perm); 
+        }
+        else
+        {
+  for(int i=perm.size()-1;i>k;i--)
+  {
+    if(perm.get(i)>perm.get(k))
+    {
+      Collections.swap(perm,i,k);
+ 
+     break;
+    }
+
+  }
+  Collections.sort(perm.subList(k+1, perm.size()));}
+  return perm;
+    }
+}
