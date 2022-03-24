3d4c38a5e11572a9e97fbfb1c477d0be51e1d94e
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-24 21:02:10 +0530

    Update Check if two trees are mirror or not.java

--- Binary Tree/Check if two trees are mirror or not.java
+++ Binary Tree/Check if two trees are mirror or not.java
@@ -11,3 +11,20 @@ class Solution {
        
     }
 }
+or
+
+class Solution {
+    boolean areMirror(Node a, Node b) {
+        // Your code here
+       if (a == null && b == null) 
+       {
+              return true ;
+        } else if (a != null && b != null) 
+        {
+return a . data == b . data&&  areMirror(a.left , b.right) &&  areMirror(a.right,b.left) ;
+}
+
+return false;
+       
+    }
+}
