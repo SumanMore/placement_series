a8fba96374b8baf9ed55ebda682d0427218e5206
Suman More <67056908+SumanMore@users.noreply.github.com>
2022-03-30 10:47:35 +0530

    Create Check even and odd .java

---
+++ Bit Manipulation/Check even and odd .java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int a=111;
+		int res=a&1;
+		if(res==1)
+		{
+		    System.out.println("odd");
+		}
+		else
+		{
+		    System.out.println("even");
+		}
+	}
+}
